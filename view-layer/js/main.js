a3116bdc1036ffd7c17ef5455d26f7bbacf2d378
CaptainSuszkow <36894691+CaptainSuszkow@users.noreply.github.com>
2019-11-02 14:13:41 +0100

    Added Login Register and User info functionalities

--- view-layer/js/main.js
+++ view-layer/js/main.js
@@ -1,1 +1,41 @@
 
+function signInButton() {
+	var authenticationData = {
+        Username : document.getElementById("inputUsername").value,
+        Password : document.getElementById("inputPassword").value,
+    };
+	
+    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+    
+	var poolData = {
+        UserPoolId : _config.cognito.userPoolId, // Your user pool id here
+        ClientId : _config.cognito.clientId, // Your client id here
+    };
+	
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+	
+    var userData = {
+        Username : document.getElementById("inputUsername").value,
+        Pool : userPool,
+    };
+	
+    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+    
+	cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function (result) {
+			var accessToken = result.getAccessToken().getJwtToken();
+            console.log(accessToken);	
+            window.open("https://dropdatabase-private.s3.amazonaws.com/Testowy/profile.html","_self");
+        },
+        onFailure: function(err) {
+            alert(err.message || JSON.stringify(err));
+        },
+    });
+  }
+
+  function signOut(){
+    if (cognitoUser != null) {
+      cognitoUser.signOut();
+      window.open('https://dropdatabase-private.s3.amazonaws.com/Testowy/login.html','_self')	  
+    }
+}
