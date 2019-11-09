cbaed451c47e684707a3dfc605005f7fa5f24973
Kamil Kowalewski <szamil128@onet.pl>
2019-11-09 12:18:56 +0100

    Initial commit of react project

---
+++ react-recruitment-system/src/main/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import Styles from "./App.module.css"
+
+export class App extends Component {
+
+  /*------------------------ FIELDS REGION ------------------------*/
+
+  /*------------------------ METHODS REGION ------------------------*/
+  
+  /*------------------------ RENDER REGION ------------------------*/
+  render() {
+    return (
+      <div className={Styles.App}>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react-recruitment-system/src/main/Routes.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import {Route, Switch} from "react-router-dom";
+
+export class Routes extends Component {
+
+  /*------------------------ FIELDS REGION ------------------------*/
+
+  /*------------------------ METHODS REGION ------------------------*/
+  render() {
+    return (
+      <Switch>
+        {/*<Route exact path="/" component={}/>*/}
+      </Switch>
+    );
+  }
+}
+
+export default Routes;
+    
