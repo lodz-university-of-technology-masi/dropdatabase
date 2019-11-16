f20d288f8bbe6fcd13b0e75c7a14a094c25f7a20
Kamil Kowalewski <szamil128@onet.pl>
2019-11-16 13:15:29 +0100

    Add home page

--- react-recruitment-system/src/page/home/Home.js
+++ react-recruitment-system/src/page/home/Home.js
@@ -6,9 +6,24 @@ export const Home = (props) => {
 
   /*------------------------ RETURN REGION ------------------------*/
   return (
-    <>
-      Home
-    </>
+    <div className="container mt-5">
+      <div class="card text-center">
+        <h3 class="card-header text-center">
+          Home
+        </h3>
+
+        <div className="card-body">
+          <h5>Decide what you want to do by clicking button in navigation section</h5>
+
+          <ul className="list-group list-group-flush mt-3">
+            <li className="list-group-item">Display Test</li>
+            <li className="list-group-item">Create Test</li>
+            <li className="list-group-item">Update Test</li>
+            <li className="list-group-item">Delete Test</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 };
 
