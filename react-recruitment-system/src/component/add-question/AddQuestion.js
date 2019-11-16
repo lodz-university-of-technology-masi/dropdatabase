13c4ada311e2e21cb068ff7d29df48f8e912b0c0
Kamil Kowalewski <szamil128@onet.pl>
2019-11-16 21:48:39 +0100

    Move parts of create test to component + add form to display

---
+++ react-recruitment-system/src/component/add-question/AddQuestion.js
@@ -0,0 +1,108 @@
+import React, {useState} from "react";
+
+export const AddQuestion = (props) => {
+
+  /*----------------------- VARIABLE REGION -----------------------*/
+  const renderSwitchButton = () => {
+    return (
+      <div className="row justify-content-center">
+        <button className="btn btn-primary" onClick={props.handleSwitchClick}>
+          Change to{props.isOpenQuestion ? " Close " : " Open "}Question
+        </button>
+      </div>
+    );
+  };
+
+  const renderOpenQuestion = () => {
+    return (
+      <form>
+        <input className="form-control mb-3" placeholder="Question"
+               name="openQuestion"
+        />
+
+        <input className="form-control mb-3" placeholder="Sample Answer"
+               name="openAnswer"
+        />
+
+        <div className="row justify-content-center mt-3">
+          <button className="btn btn-primary" onClick={props.handleSubmitOpenQuestion}>
+            Add question to test
+          </button>
+        </div>
+      </form>
+    );
+  };
+
+  //TODO ADD SUBMITING DATA FROM FORM - NOW RERENDER AFTER BUTTON CLICK AND ADD '?' TO PATH
+  const renderCloseQuestion = () => {
+    return (
+      <form>
+        <input className="form-control mb-3" placeholder="Question"
+
+        />
+
+        <div className="input-group-prepend mb-2">
+          <div className="input-group-text">
+            <input type="checkbox"/>
+          </div>
+          <input className="form-control" placeholder="Answer A"
+
+          />
+        </div>
+
+        <div className="input-group-prepend mb-2">
+          <div className="input-group-text">
+            <input type="checkbox"/>
+          </div>
+          <input className="form-control" placeholder="Answer B"
+
+          />
+        </div>
+
+        <div className="input-group-prepend mb-2">
+          <div className="input-group-text">
+            <input type="checkbox"/>
+          </div>
+          <input className="form-control" placeholder="Answer C"
+
+          />
+        </div>
+
+        <div className="input-group-prepend mb-2">
+          <div className="input-group-text">
+            <input type="checkbox"/>
+          </div>
+          <input className="form-control" placeholder="Answer D"
+
+          />
+        </div>
+
+        <div className="row justify-content-center mt-3">
+          <button className="btn btn-primary" onClick={props.handleSubmitCloseQuestion}>
+            Add question to test
+          </button>
+        </div>
+      </form>
+    );
+  };
+
+  /*------------------------ RETURN REGION ------------------------*/
+  return (
+    <>
+      <section className="card container mt-5">
+        <header className="card-body row justify-content-center">
+          <h5 className="font-weight-bold mb-3">Add New Question</h5>
+        </header>
+
+        {renderSwitchButton()}
+
+        <div className="card-body">
+          {props.isOpenQuestion ? renderOpenQuestion() : renderCloseQuestion()}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default AddQuestion;
+    
--- react-recruitment-system/src/page/create-test/CreateTest.js
+++ react-recruitment-system/src/page/create-test/CreateTest.js
@@ -1,132 +1,64 @@
 import React, {useEffect, useState} from "react";
 import {FIREBASE_PATH} from "../../constants";
 import axios from "axios";
+import uuidv4 from "uuid/v4";
 import "./CreateTest.css";
+import CreateDisplayQuestions
+  from "../../component/create-display-questions/CreateDisplayQuestions";
+import AddQuestion from "../../component/add-question/AddQuestion";
 
 export const CreateTest = (props) => {
 
   /*----------------------- VARIABLE REGION -----------------------*/
   const [isOpenQuestion, setIsOpenQuestion] = useState(false);
-  const [questionArray, setQuestionArray] = useState([
-    {"name": "sample"},
-    {"name": "sample2"},
-    {"name": "sample3"},
-  ]);
+  const [questionArray, setQuestionArray] = useState([]);
 
-  const postTest = () => {
-    axios.post(FIREBASE_PATH, questionArray).then(() => alert("Test has been sent"));
-  };
+  console.log(FIREBASE_PATH);
 
   const handleSwitchClick = () => {
     setIsOpenQuestion(!isOpenQuestion);
   };
 
-  const onClickSubmitOpenQuestion = () => {
-
+  const handleDeleteQuestion = () => {
+    console.log("delete")
   };
 
-  const onClickSubmitCloseQuestion = () => {
+  const handleSubmitOpenQuestion = (e) => {
+    console.log("open");
+    console.log(e.target.openQuestion.value);
+    let question = {
+      id: uuidv4(),
+    };
+    setQuestionArray([...questionArray, question]);
+  };
 
+  const handleSubmitCloseQuestion = () => {
+    console.log("close");
+    let question = {
+      id: uuidv4(),
+    };
+    setQuestionArray([...questionArray, question]);
+  };
+
+  const postTestToServer = () => {
+    axios.post(FIREBASE_PATH, questionArray).then(() => alert("Test has been sent"));
   };
 
   /*------------------------ RETURN REGION ------------------------*/
   return (
     <>
-      <section className="card container mt-5">
-        <header className="card-body row justify-content-center">
-          <h5 className="font-weight-bold mb-3">Add New Question</h5>
-        </header>
+      <AddQuestion
+        isOpenQuestion={isOpenQuestion}
+        handleSwitchClick={handleSwitchClick}
+        handleSubmitOpenQuestion={handleSubmitOpenQuestion}
+        handleSubmitCloseQuestion={handleSubmitCloseQuestion}
+      />
 
-        <div className="row justify-content-center">
-          <button className="btn btn-primary" onClick={handleSwitchClick}>
-            Change to {isOpenQuestion ? "Close Question" : "Open Question"}
-          </button>
-        </div>
-
-        <div className="card-body">
-          {
-            isOpenQuestion ?
-              <form>
-                <input className="form-control mb-3" placeholder="Question"
-                       onInput={(e) => console.log(e.target.value)}
-                />
-                <input className="form-control mb-3" placeholder="Sample Answer"
-                       onInput={(e) => console.log(e.target.value)}
-                />
-              </form>
-              :
-              <form>
-                <input className="form-control mb-3" placeholder="Question"
-                       onInput={(e) => console.log(e.target.value)}
-                />
-
-                <div className="input-group-prepend mb-2">
-                  <div className="input-group-text">
-                    <input type="checkbox"/>
-                  </div>
-                  <input className="form-control" placeholder="Answer A"
-                         onInput={(e) => console.log(e.target.value)}
-                  />
-                </div>
-
-                <div className="input-group-prepend mb-2">
-                  <div className="input-group-text">
-                    <input type="checkbox"/>
-                  </div>
-                  <input className="form-control" placeholder="Answer B"
-                         onInput={(e) => console.log(e.target.value)}
-                  />
-                </div>
-
-                <div className="input-group-prepend mb-2">
-                  <div className="input-group-text">
-                    <input type="checkbox"/>
-                  </div>
-                  <input className="form-control" placeholder="Answer C"
-                         onInput={(e) => console.log(e.target.value)}
-                  />
-                </div>
-
-                <div className="input-group-prepend mb-2">
-                  <div className="input-group-text">
-                    <input type="checkbox"/>
-                  </div>
-                  <input className="form-control" placeholder="Answer D"
-                         onInput={(e) => console.log(e.target.value)}
-                  />
-                </div>
-              </form>
-          }
-
-          <div className="row justify-content-center">
-            <button className="btn btn-primary">
-              Add question to test
-            </button>
-          </div>
-
-        </div>
-      </section>
-
-      <section className="card container mt-5">
-        <header className="card-body row justify-content-center">
-          <h5 className="font-weight-bold mb-3">Question in the Test</h5>
-        </header>
-
-        <ul className="list-group list-group-flush mt-3">
-          {
-            questionArray.map((it, index) => {
-              return <li className="list-group-item" key={index}>{it.name}</li>
-            })
-          }
-        </ul>
-
-        <div className="row justify-content-center">
-          <button className="btn btn-primary" onClick={postTest}>
-            Create Test
-          </button>
-        </div>
-
-      </section>
+      <CreateDisplayQuestions
+        questionArray={questionArray}
+        handleDeleteQuestion={handleDeleteQuestion}
+        postTestToServer={postTestToServer}
+      />
     </>
   );
 };
