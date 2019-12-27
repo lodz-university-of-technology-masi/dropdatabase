2b65c629ce1598652eeee53b2f9c12d9ddd7290f
Kamil Kowalewski <szamil128@onet.pl>
2019-12-27 18:48:10 +0100

    Fix issue with routes

--- react-recruitment-system/src/main/Routes.js
+++ react-recruitment-system/src/main/Routes.js
@@ -1,5 +1,5 @@
 import React, {useContext} from "react";
-import {Route, Switch} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 import {
   CANDIDATE_LIST_PATH,
   CREATE_TEST_PATH,
@@ -18,6 +18,7 @@ import DisplayTestPage from "../page/display-test-page/DisplayTestPage";
 import UpdateTestPage from "../page/update-test-page/UpdateTestPage";
 import {NotFoundPage} from "../page/not-found-page/NotFoundPage";
 import {CreateTestPage} from "../page/create-test-page/CreateTestPage";
+import PrivateRoute from "../component/private-route/PrivateRoute";
 
 export const Routes = (props) => {
 
@@ -28,25 +29,29 @@ export const Routes = (props) => {
     if (isUserLoggedIn && isRecruiter) {
       return (
         <>
-          <Route exact path={HOME_PATH} component={DisplayTestPage}/>
-          <Route exact path={UPDATE_TEST_PATH} component={UpdateTestPage}/>
-          <Route exact path={CREATE_TEST_PATH} component={CreateTestPage}/>
-          <Route exact path={REGISTER_PATH} component={RegisterPage}/>
-          <Route exact path={PROFILE_PATH} component={ProfilePage}/>
-          <Route exact path={CANDIDATE_LIST_PATH} component={CandidateListPage}/>
+          <PrivateRoute exact path={HOME_PATH} component={DisplayTestPage}/>
+          <PrivateRoute exact path={UPDATE_TEST_PATH} component={UpdateTestPage}/>
+          <PrivateRoute exact path={CREATE_TEST_PATH} component={CreateTestPage}/>
+          <PrivateRoute exact path={REGISTER_PATH} component={RegisterPage}/>
+          <PrivateRoute exact path={PROFILE_PATH} component={ProfilePage}/>
+          <PrivateRoute exact path={CANDIDATE_LIST_PATH} component={CandidateListPage}/>
         </>
       )
     } else if (isUserLoggedIn && !isRecruiter) {
       //TODO CHANGE COMPONENT IN HOME_PATH
       return (
         <>
-          <Route exact path={HOME_PATH} component={DisplayTestPage}/>
-          <Route exact path={PROFILE_PATH} component={ProfilePage}/>
+          <PrivateRoute exact path={HOME_PATH} component={DisplayTestPage}/>
+          <PrivateRoute exact path={PROFILE_PATH} component={ProfilePage}/>
         </>
       );
     } else {
       return (
-        <Route exact path={LOGIN_PATH} component={LoginPage}/>
+        <>
+          <PrivateRoute exact path={HOME_PATH} component={DisplayTestPage}/>
+          <Route exact path={LOGIN_PATH} component={LoginPage}/>
+          <Redirect to={LOGIN_PATH}/>
+        </>
       );
     }
   };
@@ -55,7 +60,8 @@ export const Routes = (props) => {
   return (
     <Switch>
       {renderPrivateRoutes(state.isUserLoggedIn, Boolean(Number(state.userAccountType)))}
-      <Route component={NotFoundPage}/>
+
+      <PrivateRoute component={NotFoundPage}/>
     </Switch>
   );
 };
